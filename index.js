b8dd5ee0ef1fd4fc5e6aa278825b8fad481f6d9a
TomasMasiulis <tomasmasiulis.1976@gmail.com>
2023-08-24 17:01:39 +0300

    2 files modified, 5 added, 5 deleted

--- index.js
+++ index.js
@@ -3,9 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>
-
-<img src="https://www.dropbox.com/scl/fi/v0ra0rl3x0uffnffaamun/object.svg?rlkey=divc5rygkxpbkybz5jgs7bkvc&dl=0"><img>
-
-
-`;
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
