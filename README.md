431d4598d8901064072efa1055af77ea64e820cb
TomasMasiulis <tomasmasiulis.1976@gmail.com>
2023-08-24 16:51:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dariui-dizainui
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jjeevd)
---
+++ index.js
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>
+
+<img src="https://www.dropbox.com/scl/fi/v0ra0rl3x0uffnffaamun/object.svg?rlkey=divc5rygkxpbkybz5jgs7bkvc&dl=0"><img>
+
+
+`;
